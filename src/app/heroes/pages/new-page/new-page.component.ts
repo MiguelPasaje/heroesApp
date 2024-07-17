22d99adac1dc4bf29b2480bc28683616fc4487c6
miguel pasaje <cartel-dy@hotmail.com>
2024-07-16 21:34:34 -0500

    getters dentro del componente

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,33 +1,44 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NewPageComponent {
   public heroForm = new FormGroup({
-    id:               new FormControl<string>(''),
-    superhero:        new FormControl<string>('',{ nonNullable:true}),
-    publisher:        new FormControl<Publisher>(Publisher.DCComics),
-    alter_ego:        new FormControl(''),
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
     first_appearance: new FormControl(''),
-    characters:       new FormControl(''),
-    alt_img:          new FormControl(''),
-
-  })
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
-  onsubmit():void{
+  constructor(private heroesService: HeroesService){}
+
+  get currentHero():Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onsubmit(): void {
     console.log({
       formIsValid: this.heroForm.valid,
       value: this.heroForm.getRawValue(),
-      value2: this.heroForm.value
-  });
+      value2: this.heroForm.value,
+    });
+
+    if (this.heroForm.invalid) return;
+
+    //this.heroesService.updateHero()
   }
 }
