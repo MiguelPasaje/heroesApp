3cbe0d4d79a9316e283af87ba343976ec0698a69
miguel pasaje <cartel-dy@hotmail.com>
2024-07-22 21:50:23 -0500

    comentario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit{
 
   constructor(private authService:AuthService){}
   ngOnInit(): void {
+    //es para mantener la sesion activa, lo cual no se hace asi, por que puede ser que carguen cosas antes que esto lo cual el usuario que intente acceder vea cosas indevidas antes de que se termine de ejecutar esto
     this.authService.checkAutentication().subscribe(()=>{
       console.log('check');
 
